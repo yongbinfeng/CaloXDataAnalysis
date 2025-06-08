23b3ab9ec54a74cc32581d288604f01c0e549e4f
yongbinfeng <ybfeng94@gmail.com>
2025-06-08 15:42:01 -0500

    study deltaT in hodoscopes

--- makePlots_HodoScopes.py
+++ makePlots_HodoScopes.py
@@ -7,12 +7,102 @@ from results.events import events_interested
 from utils.channel_map import get_hodoscope_channels
 
 ROOT.gROOT.SetBatch(True)  # Run in batch mode
+# multithread
+ROOT.ROOT.EnableImplicitMT(10)
 
 hodoscope_channels = get_hodoscope_channels()
 
 
 with open("results/hodoscope_noises.json", "r") as json_file:
     hodo_noises = json.load(json_file)
+
+
+def analyzePeak(infilename):
+    infile = ROOT.TFile(infilename, "READ")
+    if not infile or infile.IsZombie():
+        raise RuntimeError(f"Failed to open input file: {infile}")
+
+    # Create an RDataFrame from the EventTree
+    rdf = ROOT.RDataFrame("EventTree", infile)
+
+    histos1D_diff = {}
+    histos1D_left = {}
+    histos1D_right = {}
+    histos2D_left_vs_right = {}
+    histos1D_left_peak = {}
+    histos1D_right_peak = {}
+    for group, channels in hodoscope_channels.items():
+        for channel in channels:
+            rdf = rdf.Define(f"{channel}_subtracted",
+                             f"{channel} - {hodo_noises['hodoscope_' + channel]}")
+            # find the minimum index of the pulse shape
+            rdf = rdf.Define(f"{channel}_peak_position",
+                             f"ROOT::VecOps::ArgMin({channel}_subtracted)")
+            rdf = rdf.Define(f"{channel}_peak_value",
+                             f"ROOT::VecOps::Min({channel}_subtracted)")
+
+    rdfs_filtered = []
+    for group, channels in hodoscope_channels.items():
+        if group != "trigger":
+            rdf_filtered = rdf.Filter(
+                f"({channels[0]}_peak_value < -100.0 ) && ({channels[1]}_peak_value < -100.0 )"
+            )
+            # calculate the difference between left and right peaks
+            rdf_filtered = rdf_filtered.Define(f"{group}_delta_peak",
+                                               f"(int){channels[1]}_peak_position - (int){channels[0]}_peak_position")
+            rdfs_filtered.append((group, rdf_filtered))
+
+            histos1D_diff[group] = rdf_filtered.Histo1D(
+                (f"{group}_delta_peak",
+                 f"Delta Peak {group};Peak Position Difference;Counts", 2048, -1024, 1024),
+                f"{group}_delta_peak"
+            )
+            histos1D_left[group] = rdf_filtered.Histo1D(
+                (f"{group}_left_peak",
+                 f"Left Peak {group};Peak Position;Counts", 1024, 0, 1024),
+                f"{channels[0]}_peak_position"
+            )
+            histos1D_right[group] = rdf_filtered.Histo1D(
+                (f"{group}_right_peak",
+                 f"Right Peak {group};Peak Position;Counts", 1024, 0, 1024),
+                f"{channels[1]}_peak_position"
+            )
+            histos2D_left_vs_right[group] = rdf_filtered.Histo2D(
+                (f"{group}_left_peak_vs_right_peak",
+                 f"Left vs Right Peak {group};Left Peak Position;Right Peak Position;Counts",
+                 1024, 0, 1024, 1024, 0, 1024),
+                f"{channels[0]}_peak_position",
+                f"{channels[1]}_peak_position"
+            )
+            histos1D_left_peak[group] = rdf_filtered.Histo1D(
+                (f"{group}_left_peak_value",
+                 f"Left Peak Value {group};Peak Value;Counts", 2048, -2500, 1999),
+                f"{channels[0]}_peak_value"
+            )
+            histos1D_right_peak[group] = rdf_filtered.Histo1D(
+                (f"{group}_right_peak_value",
+                 f"Right Peak Value {group};Peak Value;Counts", 2048, -2500, 1999),
+                f"{channels[1]}_peak_value"
+            )
+
+    print("Writing histograms to output file...")
+    ofile = ROOT.TFile("root/hodoscope_peaks.root", "RECREATE")
+    if not ofile or ofile.IsZombie():
+        raise RuntimeError(f"Failed to open output file: {ofile}")
+    for group, hist in histos1D_diff.items():
+        hist.SetDirectory(ofile)
+        hist.Write()
+        histos1D_left[group].SetDirectory(ofile)
+        histos1D_left[group].Write()
+        histos1D_right[group].SetDirectory(ofile)
+        histos1D_right[group].Write()
+        histos2D_left_vs_right[group].SetDirectory(ofile)
+        histos2D_left_vs_right[group].Write()
+        histos1D_left_peak[group].SetDirectory(ofile)
+        histos1D_left_peak[group].Write()
+        histos1D_right_peak[group].SetDirectory(ofile)
+        histos1D_right_peak[group].Write()
+    ofile.Close()
 
 
 def analyzeHodoPulse(infilename):
@@ -76,9 +166,10 @@ def analyzeHodoPulse(infilename):
 
 
 if __name__ == "__main__":
-    files = [
-        "root/filtered_events_board1.root",
-    ]
-    for idx, input_file in enumerate(files):
-        print(f"Processing file: {input_file}")
-        analyzeHodoPulse(input_file)
+    # input_file = "root/filtered_events_board1.root"
+    # print(f"Processing file: {input_file}")
+    # analyzeHodoPulse(input_file)
+
+    input_file = "/Users/yfeng/Desktop/TTU/CaloX/Data/run316_250517140056_converted.root"
+    print(f"Processing file: {input_file}")
+    analyzePeak(input_file)
